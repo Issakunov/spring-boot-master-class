a887641350d1453f44a05361a1f555274bbc36ea
Atabek Kubanychbek <ataissakunov@gmail.com>
2022-11-05 14:47:12 -0500

    file1.txt was deleted to test pull request

--- src/main/java/jam/workspace/demo/inforapp/InfoApp.java
+++ src/main/java/jam/workspace/demo/inforapp/InfoApp.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ConfigurationProperties(prefix = "info.app")
 public class InfoApp {
-
     private String name;
     private String description;
     private String version;
